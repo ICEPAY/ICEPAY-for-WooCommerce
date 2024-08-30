98882f711ae7dd3923b9ac8b66fe1df17f60c7da
Lars <106535819+Lars-icepay@users.noreply.github.com>
2024-08-30 15:50:19 +0200

    fix meta query (#16)

--- src/Order.php
+++ src/Order.php
@@ -1,29 +1,30 @@
 <?php
 
-declare( strict_types=1 );
+declare(strict_types=1);
 
 namespace Icepay\WooCommerce;
 
-class Order {
-	public static function FindOrderByKey( string $key ): \WC_Order|null {
-		$orders = wc_get_orders( [
-			'limit'      => 1,
-			'meta_query' => [
-				'key'   => 'icepay-payment-key',
-				'value' => $key
-			]
-		] );
+class Order
+{
+    public static function FindOrderByKey(string $key): \WC_Order|null
+    {
+        $orders = wc_get_orders([
+            'limit'    => 1,
+            'meta_key' => 'icepay-payment-key',
+            'meta_value' => $key,
+            'meta_compare' => '=',
+        ]);
 
-		$orderId = current( $orders ) ? current( $orders )->get_id() : false;
+        $orderId = current($orders) ? current($orders)->get_id() : false;
 
-		if ( ! empty( $orderId ) ) {
-			$order = wc_get_order( $orderId );
-		}
+        if (!empty($orderId)) {
+            $order = wc_get_order($orderId);
+        }
 
-		if ( ! empty( $order ) && $order->get_status() !== 'trash' ) {
-			return $order;
-		}
+        if (!empty($order) && $order->get_status() !== 'trash') {
+            return $order;
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
