d91c6deb9b8610b0d2cc94a16373f159efdf0b33
Lars <106535819+Lars-icepay@users.noreply.github.com>
2025-03-19 14:25:14 +0100

    option to remove checkout icons

--- src/Icepay.php
+++ src/Icepay.php
@@ -8,6 +8,7 @@ class Icepay {
 	public const MERCHANT_ID = Integration::ID . '_general_merchant_id';
 	public const SECRET = Integration::ID . '_general_secret_code';
 	public const ENABLE_LOGS = Integration::ID . '_general_enable_logs';
+	public const SHOW_ICONS = Integration::ID . '_general_show_icons';
 	public const DESCRIPTION = Integration::ID . '_general_description';
 
 	public static function getMerchantId(): string {
@@ -20,6 +21,10 @@ class Icepay {
 
 	public static function areLogsEnabled(): bool {
 		return get_option( self::ENABLE_LOGS, 'yes' ) === 'yes';
+	}
+
+	public static function showIcons(): bool {
+		return get_option( self::SHOW_ICONS, 'yes' ) === 'yes';
 	}
 
 	public static function getDescription(): string {
