7eb6f9a8e689a390be54b8c36ef0ed67b765689c
Lars <106535819+Lars-icepay@users.noreply.github.com>
2025-08-07 11:11:41 +0200

    Fix set transaction (#31)
    * fix set transaction and i18n error message
    
    * remove expose
    
    * remove log
    
    * save

--- src/Webhook.php
+++ src/Webhook.php
@@ -49,6 +49,8 @@ class Webhook {
 		if ($orderStatus === 'pending' || $orderStatus === 'on-hold' || $orderStatus === 'cancelled' || $orderStatus === 'checkout-draft' ) {
 			$log->info( 'Updating ' . (str_replace( '{ORDER_ID}', $order->get_order_number(), Icepay::getDescription() )) . ' status to ' . $status . ' for ' . ($data['key'] ?? 'key-not-found')  );
 			$order->update_status( $status );
+			$order->set_transaction_id( $data['key'] );
+			$order->save();
 		} else {
             $log->info(
                 'Did not update '
